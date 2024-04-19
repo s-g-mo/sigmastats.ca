e172c4718d67a7ac084bb65ae8b3a35e7044aeee
s-g-mo <stephenmosher@protonmail.com>
2024-04-19 16:21:35 -0400

    rename, add func

--- web_utils.py
+++ web_utils.py
@@ -1,4 +1,6 @@
+import numpy as np
 import pandas as pd
+from data_utils import data_utils
 
 
 def beautify_event_table(df_event: pd.DataFrame):
@@ -35,13 +37,17 @@ def beautify_event_table(df_event: pd.DataFrame):
     df_event = df_event.replace('York9 FC', 'York United')
 
     # Format data types to make more presentable
-
     df_event['Prediction Briar Score'] = df_event['Prediction Briar Score'].round(3)
     df_event = df_event.fillna('')
     df_event['Home Score'] = df_event['Home Score'].astype(str)
     df_event['Away Score'] = df_event['Away Score'].astype(str)
-    df_event['Predicted Home Win Prob.'] = (df_event['Predicted Home Win Prob.'] * 100).round(2).astype(str) + '%'
-    df_event['Predicted Away Win Prob.'] = (df_event['Predicted Away Win Prob.'] * 100).round(2).astype(str) + '%'
+    df_event[['Home Score', 'Away Score']] = df_event[['Home Score', 'Away Score']].map(lambda x: x.replace('.0', ''))
+    df_event["Predicted Home Win Prob."] = df_event["Predicted Home Win Prob."].map(
+        lambda x: f"{x * 100:.2f}%" if isinstance(x, float) else ""
+    )
+    df_event["Predicted Away Win Prob."] = df_event["Predicted Away Win Prob."].map(
+        lambda x: f"{x * 100:.2f}%" if isinstance(x, float) else ""
+    )
     df_event['Prediction Correct?'] = df_event['Prediction Correct?'].astype(str)
     df_event.loc[(df_event['Prediction Correct?'] == '0.0') & (df_event['Home Score'] == ''), 'Prediction Correct?'] = ''
     df_event.loc[(df_event['Prediction Correct?'] == '1.0'), 'Prediction Correct?'] = 'True'
@@ -50,20 +56,27 @@ def beautify_event_table(df_event: pd.DataFrame):
     return df_event
 
 
-
-def beautify_rank_table(df_rank: pd.DataFrame):
-    rank_display_cols = ['team'] + [f'matchday_{i}_rank_standard' for i in range(0, 2)]
+def beautify_rank_table(df_rank: pd.DataFrame, next_matchday: int):
+    rank_display_cols = ['team'] + [f'matchday_{i}_rank_standard' for i in range(0, next_matchday)]
     df_rank = df_rank[rank_display_cols]
 
     rank_col_mapping_1 = {'team': 'Team'}
-    rank_col_mapping_2 = {f'matchday_{i}_rank_standard': f'Rank Into Matchday {i+1}' for i in range(0, 2)}
+    rank_col_mapping_2 = {f'matchday_{i}_rank_standard': f'Rank Into Matchday {i+1}' for i in range(0, next_matchday)}
     rank_display_name_mapping = {**rank_col_mapping_1, **rank_col_mapping_2}
     df_rank = df_rank.rename(columns=rank_display_name_mapping)
-    
-    df_rank = df_rank.dropna()
-    
+
     numeric_columns = [col for col in df_rank.columns if col != 'Team']
-    df_rank[numeric_columns] = df_rank[numeric_columns].round(0).astype(int)
+    df_rank[numeric_columns] = df_rank[numeric_columns].map(lambda x: round(x) if not pd.isna(x) else 0)
+    df_rank[numeric_columns] = df_rank[numeric_columns].astype(int)
+    df_rank = df_rank.replace(0, '')
+    
     df_rank = df_rank.replace('York9 FC', 'York United')
 
     return df_rank
+
+
+def determine_next_matchday():
+    url = 'https://fbref.com/en/comps/211/schedule/Canadian-Premier-League-Scores-and-Fixtures'
+    df_event = data_utils.scrape_season_schedule_FBRef(url)
+    current_matchday = int(df_event[df_event.Score.notna()].Wk.max())
+    return current_matchday + 1
